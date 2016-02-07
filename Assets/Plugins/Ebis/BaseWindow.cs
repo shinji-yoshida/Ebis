b5424a625dea8f740ff4748e33a93108ce6c486b
Shinji.Yoshida <yoshida.shinji.gm@gmail.com>
2016-02-07 18:32:55 +0900

    depend on Lockables

--- Assets/Plugins/Ebis/BaseWindow.cs
+++ Assets/Plugins/Ebis/BaseWindow.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Lockables;
 
 namespace Ebis{
 	public abstract class BaseWindow : Window {
-		List<ILockable> lockables = new List<ILockable>();
+//		List<ILockable> lockables = new List<ILockable>();
+		CompositeLockable lockables = new CompositeLockable();
 		
 		public void AddLockable(ILockable lockable){
-			lockables.Add(lockable);
+			lockables.Add (lockable);
 		}
 		
 		public void Lock () {
-			foreach(var each in lockables)
-				each.Lock();
+			lockables.Lock ();
 		}
 		
 		public void Unlock () {
-			foreach(var each in lockables)
-				each.Unlock();
+			lockables.Unlock ();
+		}
+
+		public void ForceUnlock () {
+			lockables.ForceUnlock ();
+		}
+
+		public bool IsLocked () {
+			return lockables.IsLocked ();
 		}
 	}
 }
--- Assets/Plugins/Ebis/Window.cs
+++ Assets/Plugins/Ebis/Window.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Lockables;
 
 namespace Ebis{
 	public interface Window : ILockable {
