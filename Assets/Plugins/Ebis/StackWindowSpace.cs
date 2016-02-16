e81aa710c20ebe144b02091f34009ca74cb761aa
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2016-02-16 14:28:07 +0900

    StackWindowSpace

---
+++ Assets/Plugins/Ebis/StackWindowSpace.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using gotanda;
+using System;
+
+namespace Ebis {
+	public class StackWindowSpace {
+		Stack<Window> windows;
+		Transform windowContainer;
+
+		public StackWindowSpace (Transform windowContainer)
+		{
+			this.windowContainer = windowContainer;
+		}
+		
+
+		public T Open<T>(Action<T> onInstantiated) where T : Window {
+			var prefab = WindowSystem.Instance.FindPrefab<T> ();
+			Assertion._assert_ (prefab != null);
+
+			var obj = GameObject.Instantiate(prefab);
+			obj.transform.SetParent(windowContainer, false);
+			var result = obj.GetComponent<T> ();
+
+			onInstantiated (result);
+
+			result.NotifyOnOpening ();
+			result.NotifyOnOpened ();
+
+			return result;
+		}
+	}
+}
