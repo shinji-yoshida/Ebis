d5c738ca023bbb95e3e536be3e722897f633509f
Shinji.Yoshida <yoshida.shinji.gm@gmail.com>
2016-02-24 00:05:32 +0900

    WindowDisposable

---
+++ Assets/Plugins/Ebis/ResourcesWindowCollection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace Ebis {
+	public class ResourcesWindowCollection : WindowCollection {
+		string basePath;
+
+		public ResourcesWindowCollection (string basePath) {
+			this.basePath = basePath;
+		}
+		
+
+		public T FindPrefab<T> () where T : Window {
+			return Resources.Load<T> (Path.Combine (basePath, typeof(T).Name));
+		}
+	}
+}
---
+++ Assets/Plugins/Ebis/WindowDisposable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+namespace Ebis {
+	public class WindowDisposable : IDisposable {
+		Window window;
+
+		public WindowDisposable (Window window) {
+			this.window = window;
+		}
+
+		public void Dispose () {
+			if (window == null)
+				return;
+			var cache = window;
+			window = null;
+			cache.Close ();
+		}
+	}
+}
